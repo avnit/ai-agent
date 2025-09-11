d0c0fb672603d2e86ef7bf2ed72e7b112f7d44e9
Avnit Bambah <abambah+github@gmail.com>
2025-09-11 13:17:35 -0700

    update code

--- deployment/deploy.py
+++ deployment/deploy.py
@@ -7,12 +7,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # Import the agent from the modelarmor module
 from modelarmor.agent import root_agent
-
+project = os.getenv("GOOGLE_CLOUD_PROJECT")
+location = os.getenv("GOOGLE_CLOUD_LOCATION")
+endpoint_id = os.getenv("AIP_ENDPOINT_ID")
 # Initialize the Vertex AI SDK
-vertexai.init(
-    project="YOUR_PROJECT_ID",  # Replace with your Google Cloud project ID
-    location="YOUR_REGION",     # Replace with your desired region (e.g., "us-central1")
-)
+vertexai.init()
+#    project="project",  # Replace with your Google Cloud project ID
+#    location="location",     # Replace with your desired region (e.g., "us-central1")
+# )
 
 # Build and deploy the agent
 agent = ae.AdkApp(
